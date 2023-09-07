56df24a9f520749a847627d74b94b1d1a9fea9ad
irriz-jayy <jayfelix789@gmail.com>
2023-09-08 00:14:40 +0300

    added show for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,13 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id:params[:id])
 
+    if @user
+      render json: @user, status: :ok
+    else
+      render json: {error:"User not found"}
+    end
   end
 
   def create
