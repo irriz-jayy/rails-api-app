658f546c8fef3ad3ffe2102ad964b6b1616c9bfa
irriz-jayy <jayfelix789@gmail.com>
2023-09-07 13:08:47 +0300

    added jwt to signups

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,8 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created
+      token = encode_token(user_id: @user.id)
+      render json:{ user: @user, jwt: token}, status: :created
     else
       render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
     end
